ff609d8fd0573699bca799f15349ff63c97094a9
MarcoRhayden <rhayden@arkansoftware.com>
2025-09-26 19:26:14 -0300

    chore(infra/asio): add Asio type aliases (asio, tcp, Buffer)

---
+++ src/infrastructure/net/asio/AsioTypes.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <boost/asio.hpp>
+#include <cstdint>
+#include <vector>
+
+namespace arkan::poseidon::infrastructure::net::asio_impl
+{
+namespace asio = boost::asio;
+using tcp = asio::ip::tcp;
+using Buffer = std::vector<std::uint8_t>;
+}  // namespace arkan::poseidon::infrastructure::net::asio_impl
