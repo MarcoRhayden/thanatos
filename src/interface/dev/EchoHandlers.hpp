a96949eab0834c98e33ca7d665ace990a939031a
MarcoRhayden <rhayden@arkansoftware.com>
2025-09-26 19:27:26 -0300

    feat(dev): add EchoHandlers for Query and RO (hello + echo)

---
+++ src/interface/dev/EchoHandlers.hpp
@@ -0,0 +1,57 @@
+#pragma once
+#include "application/ports/net/IConnectionHandler.hpp"
+#include "application/ports/net/ISession.hpp"
+#include "infrastructure/log/Logger.hpp"
+
+namespace arkan::poseidon::interface::dev
+{
+
+class EchoHandler final : public application::ports::net::IConnectionHandler
+{
+   public:
+    void on_connect(std::shared_ptr<application::ports::net::ISession> s) override
+    {
+        using arkan::poseidon::infrastructure::log::Logger;
+        Logger::info(std::string("[Query] connect: ") + s->remote_endpoint());
+        const char msg[] = "hello from QueryTcpServer\n";
+        s->send(std::span<const std::uint8_t>(reinterpret_cast<const std::uint8_t*>(msg),
+                                              sizeof(msg) - 1));
+    }
+    void on_data(std::shared_ptr<application::ports::net::ISession> s,
+                 std::span<const std::uint8_t> bytes) override
+    {
+        s->send(bytes);
+    }
+    void on_disconnect(std::shared_ptr<application::ports::net::ISession> s,
+                       const std::error_code&) override
+    {
+        using arkan::poseidon::infrastructure::log::Logger;
+        Logger::info(std::string("[Query] disconnect: ") + s->remote_endpoint());
+    }
+};
+
+class EchoHandlerRo final : public application::ports::net::IConnectionHandler
+{
+   public:
+    void on_connect(std::shared_ptr<application::ports::net::ISession> s) override
+    {
+        using arkan::poseidon::infrastructure::log::Logger;
+        Logger::info(std::string("[RO] connect: ") + s->remote_endpoint());
+        const char msg[] = "hello from RoTcpServer\n";
+        s->send(std::span<const std::uint8_t>(reinterpret_cast<const std::uint8_t*>(msg),
+                                              sizeof(msg) - 1));
+    }
+    void on_data(std::shared_ptr<application::ports::net::ISession> s,
+                 std::span<const std::uint8_t> bytes) override
+    {
+        s->send(bytes);
+    }
+    void on_disconnect(std::shared_ptr<application::ports::net::ISession> s,
+                       const std::error_code&) override
+    {
+        using arkan::poseidon::infrastructure::log::Logger;
+        Logger::info(std::string("[RO] disconnect: ") + s->remote_endpoint());
+    }
+};
+
+}  // namespace arkan::poseidon::interface::dev
