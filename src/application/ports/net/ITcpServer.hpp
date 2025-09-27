83dbf5ab11fe3eb6a5e37381cb140c30359b1b9f
MarcoRhayden <rhayden@arkansoftware.com>
2025-09-27 01:10:08 -0300

    style(ports/net): inline virtual dtor and const accessors on ITcpServer

--- src/application/ports/net/ITcpServer.hpp
+++ src/application/ports/net/ITcpServer.hpp
@@ -1,21 +1,20 @@
 #pragma once
 
 #include <cstdint>
-#include <memory>
 
 namespace arkan::poseidon::application::ports::net
 {
 
-class IConnectionHandler;
-
 class ITcpServer
 {
    public:
-    virtual ~ITcpServer() = default;
+    virtual ~ITcpServer() = 0;
     virtual void start() = 0;
     virtual void stop() = 0;
     virtual std::uint16_t port() const = 0;
     virtual bool is_running() const = 0;
 };
+
+inline ITcpServer::~ITcpServer() = default;
 
 }  // namespace arkan::poseidon::application::ports::net
