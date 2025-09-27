7d42923eee87a5e027b84596aafd53bb759e3b9b
MarcoRhayden <rhayden@arkansoftware.com>
2025-09-27 03:45:03 -0300

    feat(interface): add LoginHandlerDev bridging protocol packets to LoginService

--- src/interface/dev/LoginHandlerDev.hpp
+++ src/interface/dev/LoginHandlerDev.hpp
@@ -1,50 +1,78 @@
 #pragma once
+
 #include <memory>
 #include <span>
 #include <system_error>
+#include <unordered_map>
+#include <vector>
 
 #include "application/ports/net/IConnectionHandler.hpp"
 #include "application/ports/net/ISession.hpp"
 #include "application/services/ILoginService.hpp"
 #include "domain/protocol/Codec.hpp"
 #include "domain/protocol/Parser.hpp"
+#include "infrastructure/log/Logger.hpp"
 
 namespace arkan::poseidon::interface::dev
 {
 
-class LoginHandlerDev : public application::ports::net::IConnectionHandler
+namespace ports = arkan::poseidon::application::ports::net;
+namespace svc = arkan::poseidon::application::services;
+namespace proto = arkan::poseidon::domain::protocol;
+using arkan::poseidon::infrastructure::log::Logger;
+
+class LoginHandlerDev final : public ports::IConnectionHandler
 {
    public:
-    explicit LoginHandlerDev(std::unique_ptr<application::services::ILoginService> svc,
-                             std::size_t maxpkt = 4 * 1024 * 1024)
-        : svc_(std::move(svc)), parser_(maxpkt)
+    explicit LoginHandlerDev(std::shared_ptr<svc::ILoginService> service)
+        : service_(std::move(service))
     {
     }
 
-    void on_connect(std::shared_ptr<application::ports::net::ISession>) override {}
-    void on_disconnect(std::shared_ptr<application::ports::net::ISession>,
-                       const std::error_code&) override
+    void on_connect(std::shared_ptr<ports::ISession> s) override
     {
+        Logger::info("[login] connect: " + s->remote_endpoint());
+
+        auto& st = sessions_[s.get()];
+        st.session = std::move(s);
     }
 
-    void on_data(std::shared_ptr<application::ports::net::ISession> s,
-                 std::span<const std::uint8_t> bytes) override
+    void on_data(std::shared_ptr<ports::ISession> s, std::span<const std::uint8_t> bytes) override
     {
-        parser_.feed(bytes);
-        for (auto& p : parser_.drain())
+        auto it = sessions_.find(s.get());
+        if (it == sessions_.end()) return;
+        auto& st = it->second;
+
+        st.parser.feed(bytes);
+
+        auto packets = st.parser.drain();
+        for (auto& p : packets)
         {
-            auto replies = svc_->handle(p);
-            for (auto& r : replies)
+            auto outs = service_->handle(p);
+            for (auto& outp : outs)
             {
-                auto buf = domain::protocol::Encode(r);
-                s->send(std::span<const std::uint8_t>(buf.data(), buf.size()));
+                auto buf = proto::Encode(outp);
+                st.session->send(buf);
             }
         }
     }
 
+    void on_disconnect(std::shared_ptr<ports::ISession> s, const std::error_code& ec) override
+    {
+        Logger::info("[login] disconnect: " + s->remote_endpoint() +
+                     (ec ? (" ec=" + ec.message()) : ""));
+        sessions_.erase(s.get());
+    }
+
    private:
-    std::unique_ptr<application::services::ILoginService> svc_;
-    domain::protocol::Parser parser_;
+    struct ConnState
+    {
+        std::shared_ptr<ports::ISession> session;
+        proto::Parser parser;  // default-constructible
+    };
+
+    std::unordered_map<ports::ISession*, ConnState> sessions_;
+    std::shared_ptr<svc::ILoginService> service_;
 };
 
 }  // namespace arkan::poseidon::interface::dev
