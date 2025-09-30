b14d520b4dedd295906ec7e5ee9ec8f51560ee36
MarcoRhayden <rhayden@arkansoftware.com>
2025-09-30 19:45:31 -0300

    feat(ro): compose QueryServer + GameGuardBridge in RagnarokServer

--- src/interface/ragnarok/RagnarokServer.cpp
+++ src/interface/ragnarok/RagnarokServer.cpp
@@ -5,36 +5,45 @@ namespace arkan::poseidon::interface::ro
 
 RagnarokServer::RagnarokServer(boost::asio::io_context& io,
                                std::shared_ptr<SessionRegistry> registry, const apc::Config& cfg)
-    : registry_(std::move(registry)),
+    : io_(io),
+      registry_(std::move(registry)),
       cfg_(cfg),
+      qhost_(cfg_.fakeIP),
+      qport_(cfg_.query_port),
       login_handler_(std::make_shared<LoginHandler>(
-          registry_, cfg_, [](const std::string& msg) { Logger::debug("[login] " + msg); })),
+          registry_, cfg_, [](const std::string& m) { Logger::debug("[login] " + m); })),
       char_handler_(std::make_shared<CharHandler>(
-          registry_, cfg_, [](const std::string& msg) { Logger::debug("[char] " + msg); })),
-      login_srv_(asio_impl::MakeTcpServer(io, cfg_.login_port, login_handler_, cfg_.fakeIP)),
-      char_srv_(asio_impl::MakeTcpServer(io, cfg_.char_port, char_handler_, cfg_.fakeIP))
+          registry_, cfg_, [](const std::string& m) { Logger::debug("[char] " + m); })),
+      login_srv_(asio_impl::MakeTcpServer(io_, cfg_.login_port, login_handler_, cfg_.fakeIP)),
+      char_srv_(asio_impl::MakeTcpServer(io_, cfg_.char_port, char_handler_, cfg_.fakeIP))
 {
-    Logger::debug("[ragnarok] RagnarokServer constructed: login=" +
-                  std::to_string(cfg_.login_port) + " char=" + std::to_string(cfg_.char_port));
+    query_server_ = std::make_unique<interface::query::QueryServer>(io_, qhost_, qport_);
+
+    // Bridge GameGuard (injects into handlers)
+    gg_bridge_ = std::make_unique<application::services::GameGuardBridge>(*query_server_);
+    login_handler_->set_gg_bridge(gg_bridge_.get());
+    char_handler_->set_gg_bridge(gg_bridge_.get());
+
+    Logger::debug("[ragnarok] constructed: login=" + std::to_string(cfg_.login_port) +
+                  " char=" + std::to_string(cfg_.char_port) + " query=" + qhost_ + ":" +
+                  std::to_string(qport_));
 }
 
 void RagnarokServer::start()
 {
+    // Start the Poseidon channel
+    if (query_server_) query_server_->start();
     if (login_srv_) login_srv_->start();
     if (char_srv_) char_srv_->start();
-    Logger::debug("[ragnarok] RagnarokServer start(): listeners active");
-}
-
-void RagnarokServer::iterate()
-{
-    // no-op for now; hook point for timers/housekeeping
+    Logger::debug("[ragnarok] start(): listeners active");
 }
 
 void RagnarokServer::stop()
 {
     if (login_srv_) login_srv_->stop();
     if (char_srv_) char_srv_->stop();
-    Logger::debug("[ragnarok] RagnarokServer stop(): listeners closed");
+    if (query_server_) query_server_->stop();
+    Logger::debug("[ragnarok] stop(): listeners closed");
 }
 
 }  // namespace arkan::poseidon::interface::ro
--- src/interface/ragnarok/RagnarokServer.hpp
+++ src/interface/ragnarok/RagnarokServer.hpp
@@ -3,10 +3,12 @@
 #include <memory>
 #include <string>
 
+#include "application/services/GameGuardBridge.hpp"
 #include "application/state/SessionRegistry.hpp"
 #include "infrastructure/config/Config.hpp"
 #include "infrastructure/log/Logger.hpp"
 #include "infrastructure/net/asio/AsioTcpServer.hpp"
+#include "interface/query/QueryServer.hpp"
 #include "interface/ragnarok/char/CharHandler.hpp"
 #include "interface/ragnarok/login/LoginHandler.hpp"
 
@@ -25,20 +27,23 @@ class RagnarokServer final
    public:
     RagnarokServer(boost::asio::io_context& io, std::shared_ptr<SessionRegistry> registry,
                    const apc::Config& cfg);
-
     void start();
-    void iterate();
     void stop();
 
    private:
+    boost::asio::io_context& io_;
     std::shared_ptr<SessionRegistry> registry_;
     apc::Config cfg_;
+    std::string qhost_;
+    uint16_t qport_{};
 
     std::shared_ptr<LoginHandler> login_handler_;
     std::shared_ptr<CharHandler> char_handler_;
 
     std::unique_ptr<ports_net::ITcpServer> login_srv_;
     std::unique_ptr<ports_net::ITcpServer> char_srv_;
+    std::unique_ptr<interface::query::QueryServer> query_server_;
+    std::unique_ptr<application::services::GameGuardBridge> gg_bridge_;
 };
 
 }  // namespace arkan::poseidon::interface::ro
